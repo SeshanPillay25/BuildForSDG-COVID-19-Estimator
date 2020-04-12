8eb7c175ca6d95bec4866b75ccdc0351ae3b833f
Carike Blignaut-Staden <carikeyvs@gmail.com>
2020-04-12 16:55:27 +0200

    Correct constants and shortend lines where too long

--- src/impact.js
+++ src/impact.js
@@ -9,7 +9,8 @@ const impact = (data) => {
   // Estimated number of severe positive cases that will require hospitalization to recover
   const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
   // Determine the number of available beds exclude the servere cases
-  const hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds) - severeCasesByRequestedTime);
+  const hospitalBedsByRequestedTime = Math.trunc(
+    (0.35 * data.totalHospitalBeds) - severeCasesByRequestedTime);
 
   // Challenge 3
   // Number of severe positive cases require ICU care
@@ -18,8 +19,8 @@ const impact = (data) => {
   const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
 
   // Estimated financial loss for region
-  const dollarsInFlight = infectionsByRequestedTime * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation * convertToDays(data);
-  dollarsInFlight = (dollarsInFlight).toFixed(2);
+  const regionNum = data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation;
+  const dollarsInFlight = (infectionsByRequestedTime * regionNum * convertToDays(data)).toFixed(2);
 
   return {
     currentlyInfected,
--- src/severeImpact.js
+++ src/severeImpact.js
@@ -10,7 +10,8 @@ const severeImpact = (data) => {
   // Estimated number of severe positive cases that will require hospitalization to recover
   const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
   // Determine the number of available beds exclude the servere cases
-  const hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds) - severeCasesByRequestedTime);
+  const hospitalBedsByRequestedTime = Math.trunc(
+    (0.35 * data.totalHospitalBeds) - severeCasesByRequestedTime);
 
   // Challenge 3
   // Number of severe positive cases require ICU care
@@ -19,7 +20,8 @@ const severeImpact = (data) => {
   const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
 
   // Estimated financial loss for region
-  const dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation * convertToDays(data)).toFixed(2);
+  const regionNum = data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation;
+  const dollarsInFlight = (infectionsByRequestedTime * regionNum * convertToDays(data)).toFixed(2);
 
   return {
     currentlyInfected,
