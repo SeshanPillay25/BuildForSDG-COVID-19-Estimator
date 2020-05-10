219d78795ddcf545393307fadc293ed366e61236
Restyled.io <commits@restyled.io>
2020-05-10 11:14:55 +0000

    Restyled by clang-format

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,7 @@
 import impact from './impact.js';
 import severeImpact from './severeImpact.js';
 
-const covid19ImpactEstimator = (data) => ({
-  data,
-  impact: impact(data),
-  severeImpact: severeImpact(data)
-});
+const covid19ImpactEstimator = (data) =>
+    ({data, impact : impact(data), severeImpact : severeImpact(data)});
 
 export default covid19ImpactEstimator;
